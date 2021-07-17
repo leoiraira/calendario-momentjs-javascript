551d51092737038b37f5175d4e97ecef4597f7e4
leoiraira <leoiraira@udec.cl>
2021-07-17 01:14:11 -0400

    main

--- js/calendar.js
+++ js/calendar.js
@@ -130,13 +130,19 @@ class Calendar {
                 if (elTarget.classList.contains('grid__cell--disabled') || elTarget.classList.contains('grid__cell--selected')) {
                     return;
                 }
-                // Deselecionar la celda anterior
+                /*// Deselecionar la celda anterior
                 let selectedCell = this.elGridBody.querySelector('.grid__cell--selected');
                 if (selectedCell) {
                     selectedCell.classList.remove('grid__cell--selected');
-                }
+                }*/
+                //deseleccionar al dar otro click
+                let selectedCell = this.elGridBody.querySelector('.grid__cell--selected');
+          
+                if (selectedCell) {
+                    elTarget.classList.remove('grid__cell--selected');}
                 // Selecionar la nueva celda
                 elTarget.classList.add('grid__cell--selected');
+                
                 this.selectedDate = this.cells[parseInt(elTarget.dataset.cellId)].date;
                 // Lanzar evento change
                 this.elCalendar.dispatchEvent(new Event('change'));
